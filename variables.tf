e77513e3854baeac109bdecd1f0b4d471def68db
Peter Phan <peterphan@hotmail.com>
2019-08-28 22:17:59 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,13 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-west-1"
+  default = "us-west-2"
 }
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
   #default = "ami-2e1ef954"
-  default = "ami-056d04da775d124d7"
+  #default = "ami-056d04da775d124d7"
+  default = "ami-09a3d8a7177216dcf" #us-west-2
 }
 
 variable "instance_type" {

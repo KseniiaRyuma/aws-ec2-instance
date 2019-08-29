a8386f76a3bbbfc4d92beb4f9ee18b27868dd081
Peter Phan <pphan@hashicorp.com>
2019-08-29 14:04:01 -0700

    tf fmt

--- main.tf
+++ main.tf
@@ -7,13 +7,14 @@ provider "aws" {
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+  ami               = var.ami_id
+  instance_type     = var.instance_type
   availability_zone = "${var.aws_region}b"
 
   tags = {
-    Name = var.name
-    Owner = var.owner
+    Name       = var.name
+    Owner      = var.owner
     DeployedBy = "Terraform"
+    TTL        = "24h"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-west-2"
+  default     = "us-west-2"
 }
 
 variable "ami_id" {
@@ -12,12 +12,12 @@ variable "ami_id" {
 
 variable "instance_type" {
   description = "type of EC2 instance to provision."
-  default = "t3.micro"
+  default     = "t3.micro"
 }
 
 variable "name" {
   description = "name to pass to Name tag"
-  default = "Phan-Provisioned by Terraform"
+  default     = "Phan-Provisioned by Terraform"
 }
 
 variable "owner" {

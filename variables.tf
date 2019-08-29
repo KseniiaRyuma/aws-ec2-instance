644747cb2b6e7fc62ab11a0d0e4658466bc7cc75
Peter Phan <peterphan@hotmail.com>
2019-08-28 22:07:04 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,12 +5,12 @@ variable "aws_region" {
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-2e1ef954"
+  default = "ami-056d04da775d124d7"
 }
 
 variable "instance_type" {
   description = "type of EC2 instance to provision."
-  default = "t2.micro"
+  default = "t3.micro"
 }
 
 variable "name" {

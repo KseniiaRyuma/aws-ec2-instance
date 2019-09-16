551f556b470c69ff70bf069e6907b1a1b5b4d0e7
Peter Phan <pphan@hashicorp.com>
2019-09-16 15:07:18 -0700

    Separated SG rules from SG

--- main.tf
+++ main.tf
@@ -28,25 +28,21 @@ resource "aws_security_group" "bastion" {
 resource "aws_security_group_rule" "allow_ssh_in" {
   type = "ingress"
   security_group_id = "aws_security_group.bastion.id"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"
-      ]
-  }
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
 }
 resource "aws_security_group_rule" "allow_all_out" {
   type = "egress"
   security_group_id = "aws_security_group.bastion.id"
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = [
-      "0.0.0.0/0"
-      ]
-  }
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
 }
 

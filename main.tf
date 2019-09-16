2ac3f2cf4ff518e8f2c97ca43727611e79708fed
Peter Phan <pphan@hashicorp.com>
2019-09-16 09:54:18 -0700

    added SG

--- main.tf
+++ main.tf
@@ -20,20 +20,20 @@ resource "aws_instance" "ubuntu" {
 }
 
 resource "aws_security_group" "ssh" {
-  name    = "allow_ssh"
+  name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
   ingress {
-    from_port = 443
-    to_port   = 443
-    procotol  = "-1"
-    cidr_blocks = [ "0.0.0.0/0 "]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0 "]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = [ "0.0.0.0/0 "]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0 "]
   }
   tags = {
     Name = "SSH"

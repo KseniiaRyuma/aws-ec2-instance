410ca32242acba89ce2304f87d8c2a1d18893c01
Peter Phan <pphan@hashicorp.com>
2019-09-16 10:12:46 -0700

    Fixed SG ingress: 443 to 22

--- main.tf
+++ main.tf
@@ -23,8 +23,8 @@ resource "aws_security_group" "ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 22
+    to_port     = 22
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }

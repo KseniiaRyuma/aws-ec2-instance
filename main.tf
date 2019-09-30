be3f94fca36c0c19d524f4e4b90aeb59fdf8b7b0
Peter Phan <pphan@hashicorp.com>
2019-09-30 14:06:19 -0700

    sg ssh to 0

--- main.tf
+++ main.tf
@@ -28,8 +28,8 @@ resource "aws_security_group" "bastion" {
 resource "aws_security_group_rule" "allow_ssh_in" {
   type = "ingress"
   security_group_id = aws_security_group.bastion.id
-  from_port   = 22
-  to_port     = 22
+  from_port   = 0
+  to_port     = 0
   protocol    = "tcp"
   cidr_blocks = [
     "0.0.0.0/0"
